62e074d23ec7eab83c615c568c9357a25e962c84
vipinray <sajrajsingh@gmail.com>
2016-09-19 10:50:51 +0530

    new line is added in local branch by developer

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ Class Main
 		System.out.println("Hello world");
 		System.out.println("modify line in Git Foler..");
 		System.out.println("modigy new line in Remote Repository");
+		System.out.println("this is branch line create by developer in local");
 
 	}
 }
